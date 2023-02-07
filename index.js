e6f0185264585257f6af82dbb3ddff9fcf5023b9
Fozan-Developer <nazar.korkin@mail.ru>
2023-02-07 23:49:24 +0300

    update

---
+++ index.js
@@ -0,0 +1,5 @@
+const number = require('./lib/number/index.js');
+const date = require('./lib/date/index.js');
+const symbol = require('./lib/symbol/index.js');
+
+module.exports = { number, date, symbol };
